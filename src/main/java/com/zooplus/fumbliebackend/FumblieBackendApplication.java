28e86656caf5647f20fb8d7b4b008a8a8b3dc93c
Igor Ivaniuk <igor.ivaniuk@zooplus.com>
2017-07-24 10:27:06 +0200

    Empty skeleton

---
+++ src/main/java/com/zooplus/fumbliebackend/FumblieBackendApplication.java
@@ -0,0 +1,12 @@
+package com.zooplus.fumbliebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FumblieBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FumblieBackendApplication.class, args);
+	}
+}
