05cab5a61340d376ee602a4a0ba28952fadf2589
Igor Ivaniuk <igor.ivaniuk@zooplus.com>
2017-07-24 15:46:41 +0200

    Swagger

--- src/main/java/com/zooplus/fumbliebackend/configuration/WebConfiguration.java
+++ src/main/java/com/zooplus/fumbliebackend/configuration/WebConfiguration.java
@@ -4,14 +4,22 @@ import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class WebConfiguration {
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+
     @Bean
     ServletRegistrationBean h2servletRegistration() {
         ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
         registrationBean.addUrlMappings("/console/*");
         registrationBean.addInitParameter("webAllowOthers", "true");
         return registrationBean;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:/swagger-ui.html");
     }
 }
