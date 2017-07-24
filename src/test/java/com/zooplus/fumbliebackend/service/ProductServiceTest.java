220c6455faacaf04514ba0c3c00cb158dba45640
Edgard Ndouna <Edgard.Ndouna@zooplus.com>
2017-07-24 15:56:35 +0200

    Add unit tests for for the new endpoint

--- src/test/java/com/zooplus/fumbliebackend/service/ProductServiceTest.java
+++ src/test/java/com/zooplus/fumbliebackend/service/ProductServiceTest.java
@@ -1,12 +1,16 @@
 package com.zooplus.fumbliebackend.service;
 
 
+import com.zooplus.fumbliebackend.error.exception.InvalidProductIdException;
+import com.zooplus.fumbliebackend.error.exception.ProductNotFoundException;
+import com.zooplus.fumbliebackend.message.product.ProductResponse;
 import com.zooplus.fumbliebackend.message.product.ProductsResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
 
 import static org.junit.Assert.assertTrue;
 
@@ -14,13 +18,31 @@ import static org.junit.Assert.assertTrue;
 @SpringBootTest
 public class ProductServiceTest {
 
-    @Autowired
+    @Resource
     private ProductService underTest;
 
     @Test
     public void shouldGetAllProducts() {
         ProductsResponse response = underTest.getAllProducts();
         assertTrue(response.getProducts().size() > 0);
+    }
+
+    @Test
+    public void shouldRetrieveAnExistingProduct() {
+        ProductResponse response = underTest.getProductById("1");
+
+        assertTrue(response.getProduct() != null);
+        assertTrue(response.getProduct().getId() > 0);
+    }
+
+    @Test(expected = InvalidProductIdException.class)
+    public void shouldThrowInvalidProductIdException() {
+        underTest.getProductById("string");
+    }
+
+    @Test(expected = ProductNotFoundException.class)
+    public void shouldThrowProductNotFoundException() {
+        underTest.getProductById("99999");
     }
 
 }
