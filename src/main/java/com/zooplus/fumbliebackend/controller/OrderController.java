e690842df6bcfc01d9ef8054caa8bdbf3992aacf
Igor Ivaniuk <igor.ivaniuk@zooplus.com>
2017-07-24 15:20:08 +0200

    CORS for both controllers

--- src/main/java/com/zooplus/fumbliebackend/controller/OrderController.java
+++ src/main/java/com/zooplus/fumbliebackend/controller/OrderController.java
@@ -1,7 +1,9 @@
 package com.zooplus.fumbliebackend.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
+@CrossOrigin
 public class OrderController {
 }
